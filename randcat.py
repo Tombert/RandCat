850464de61237a7fae64219a39e9c937f7d40c01
Thomas Gebert <thomas@gebert.sexy>
2016-02-03 17:27:25 -0500

    Make it so we're using our own seed.

--- randcat.py
+++ randcat.py
@@ -1,7 +1,16 @@
 #! /usr/bin/python3
-import random
+import calendar
+import time
 
-random.seed() # this initializes with the Date, which I think is a novel enough seed
+seed = calendar.timegm(time.gmtime()) # We'll use the epoch time as a seed.
 
+def random (seed):
+    seed2 =  (seed*297642 + 83782)/70000
+    return int(seed2) % 70000;
+    
+    
+p = seed
 while True: # if we're going with a mimicing of cat /dev/random, it'll pretty much just go until it's killed
-    print(chr(random.getrandbits(8)), end='') 
+    p = random(p)
+    print(chr(p % 256), end="")
+    p = p % 4000
