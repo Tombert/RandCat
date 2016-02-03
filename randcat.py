1c01b9e794445242c450534d1615a9dc755b89da
Thomas Gebert <thomas@gebert.sexy>
2016-02-03 15:43:49 -0500

    Add some comments and a shebang on top.

--- randcat.py
+++ randcat.py
@@ -1,6 +1,7 @@
+#! /usr/bin/python3
 import random
 
-random.seed()
+random.seed() # this initializes with the Date, which I think is a novel enough seed
 
-while True:
-    print(chr(random.getrandbits(8)), end='')
+while True: # if we're going with a mimicing of cat /dev/random, it'll pretty much just go until it's killed
+    print(chr(random.getrandbits(8)), end='') 
